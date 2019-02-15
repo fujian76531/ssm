425509d51b98c26e6f148e74fb71743256917e05
launcher <laun_cher@163.com>
2019-02-15 17:11:50 +0800

    修改log4j.xml

---
+++ src/main/java/com/sdepc/dao/MessageMapperTest.java
@@ -0,0 +1,45 @@
+package com.sdepc.dao;
+
+import com.sdepc.entity.Message;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 437862485@qq.com on 2019/2/15.
+ */
+public class MessageMapperTest {
+
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private MessageMapper mapper;
+
+    @Before
+    public void setUp() throws Exception {
+        // 加载spring配置文件
+        applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
+        // 导入需要测试的
+        mapper = applicationContext.getBean(MessageMapper.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void insert() throws Exception {
+        Message message = new Message();
+        message.setCommand("吃饭");
+        message.setContent("睡觉");
+        message.setDescription("打豆豆");
+        int result = mapper.insert(message);
+        System.out.println(result);
+        assert (result == 1);
+    }
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 437862485@qq.com
+  Date: 2019/2/15
+  Time: 16:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+    <p>hello world</p>
+</body>
+</html>
